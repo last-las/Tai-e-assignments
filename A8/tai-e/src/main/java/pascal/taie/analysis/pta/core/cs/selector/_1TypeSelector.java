e9f30182e5868f6bc18c8928bb5f12db83d5dcd3
las <478942543@qq.com>
2022-08-13 12:41:19 +0800

    copy selector codes from Assignment 6 to Assignment 8

--- A8/tai-e/src/main/java/pascal/taie/analysis/pta/core/cs/selector/_1TypeSelector.java
+++ A8/tai-e/src/main/java/pascal/taie/analysis/pta/core/cs/selector/_1TypeSelector.java
@@ -29,6 +29,7 @@ import pascal.taie.analysis.pta.core.cs.element.CSMethod;
 import pascal.taie.analysis.pta.core.cs.element.CSObj;
 import pascal.taie.analysis.pta.core.heap.Obj;
 import pascal.taie.language.classes.JMethod;
+import pascal.taie.language.type.Type;
 
 /**
  * Implementation of 1-type sensitivity.
@@ -43,18 +44,19 @@ public class _1TypeSelector implements ContextSelector {
     @Override
     public Context selectContext(CSCallSite callSite, JMethod callee) {
         // TODO - finish me
-        return null;
+        return callSite.getContext();
     }
 
     @Override
     public Context selectContext(CSCallSite callSite, CSObj recv, JMethod callee) {
         // TODO - finish me
-        return null;
+        Type type = recv.getObject().getContainerType();
+        return ListContext.make(type);
     }
 
     @Override
     public Context selectHeapContext(CSMethod method, Obj obj) {
         // TODO - finish me
-        return null;
+        return getEmptyContext();
     }
 }
